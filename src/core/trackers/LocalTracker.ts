9f9f48f6e2d7c7cbd9b60ff52b56632778bf3acd
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-06-24 12:36:07 +0300

    chore: add trackers infra

---
+++ src/core/trackers/LocalTracker.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import { createTracker, TrackerInterface } from './tracker-utils'
+
+export const LocalTracker: TrackerInterface = createTracker({
+  async init(): Promise<void> {
+    console.log('Init tracker')
+  },
+
+  async identify(vkId: number): Promise<void> {
+    console.log(`linked session with vkId ${vkId}`)
+  },
+
+  async reachGoal(
+    name: string,
+    params?: Record<string, unknown>
+  ): Promise<void> {
+    if (!params) console.log(`Reach goal ${name}`)
+    else console.log(`Reach goal ${name} with params`, params)
+  },
+})
---
+++ src/core/trackers/trackers.ts
@@ -0,0 +1,34 @@
+import { Utils } from '../../Utils'
+import { LocalTracker } from './LocalTracker'
+
+const registeredTrackers = Utils.isProductionMode ? [] : [LocalTracker]
+
+function call(method: string, ...args: Array<unknown>) {
+  registeredTrackers.forEach((tracker) => {
+    if (
+      tracker.AUTOMATIC_OPERATIONS[0] === '*' ||
+      tracker.AUTOMATIC_OPERATIONS.includes(method)
+    ) {
+      // @ts-ignore
+      const fn = tracker[method]
+      if (typeof fn !== 'function') {
+        throw new Error(`Invalid tracker method: ${method}`)
+      }
+      fn(...args)
+    }
+  })
+}
+
+export const trackers = {
+  identify(vkId: number): void {
+    call('identify', vkId)
+  },
+
+  init(): void {
+    call('init')
+  },
+
+  reachGoal(name: string, params?: Record<string, unknown>): void {
+    call('reachGoal', name, params)
+  },
+}
