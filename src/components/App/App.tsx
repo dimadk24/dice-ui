35460c721447b0d4c823bf1e79a3b36e295a8bea
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-06-26 19:44:51 +0300

    feat: define views

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,17 +8,41 @@ import {
 } from '@vkontakte/vkui'
 import { useEffect, useState } from 'react'
 import { DELAY_BEFORE_LOADER } from '../utils/Loader'
+import {
+  Icon28Dice5Outline,
+  Icon24GearOutline,
+  Icon28HelpOutline,
+} from '@vkontakte/icons'
 
 type Props = {
   loadingUser: boolean
   popout: JSX.Element | null
 }
 
+const views = [
+  {
+    title: 'Главная',
+    Icon: Icon28Dice5Outline,
+    storyId: 'dice',
+    renderView: (): JSX.Element | string => 'dice home',
+  },
+  {
+    title: 'Настройки',
+    Icon: Icon24GearOutline,
+    storyId: 'settings',
+    renderView: (): JSX.Element | string => 'settings home',
+  },
+  {
+    title: 'Помощь',
+    Icon: Icon28HelpOutline,
+    storyId: 'help',
+    renderView: (): JSX.Element | string => 'help home',
+  },
+]
+
 export function App({ loadingUser, popout }: Props): JSX.Element {
   const [loadingTooLong, setLoadingTooLong] = useState(false)
   const [activeStory, setActiveStory] = useState('dice')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [activePanel, setActivePanel] = useState('home')
 
   useEffect(() => {
     let timerId: number
@@ -40,19 +64,29 @@ export function App({ loadingUser, popout }: Props): JSX.Element {
       activeStory={activeStory}
       tabbar={
         <Tabbar>
-          <TabbarItem
-            text="Главная"
-            selected={activeStory === 'dice'}
-            onClick={() => {
-              setActiveStory('game')
-            }}
-          />
+          {views.map(({ Icon, storyId, title }) => (
+            <TabbarItem
+              text={title}
+              selected={activeStory === storyId}
+              onClick={() => setActiveStory(storyId)}
+              key={storyId}
+            >
+              <Icon />
+            </TabbarItem>
+          ))}
         </Tabbar>
       }
     >
-      <View id="dice" activePanel={activePanel} popout={popoutToRender}>
-        <Panel id="home">main</Panel>
-      </View>
+      {views.map(({ storyId, renderView }) => (
+        <View
+          id={storyId}
+          activePanel={`${storyId}-home`}
+          popout={popoutToRender}
+          key={storyId}
+        >
+          <Panel id={`${storyId}-home`}>{renderView()}</Panel>
+        </View>
+      ))}
     </Epic>
   )
 }
