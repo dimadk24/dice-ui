fc4e51ffef8f907bf9ba781a81f85747f111f5bd
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-07-14 23:10:42 +0300

    feat: create more realistic view
    Move and rotate camera
    Change dice scale, surface size

--- src/components/panels/DiceHome/react-three-components/Surface/Surface.tsx
+++ src/components/panels/DiceHome/react-three-components/Surface/Surface.tsx
@@ -6,13 +6,13 @@ import { useLoader } from '@react-three/fiber'
 export function Surface(): JSX.Element {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, -2, 0],
+    position: [0, 0, 0],
   }))
   const texture = useLoader(TextureLoader, woodenTableTextureUrl)
 
   return (
     <mesh ref={ref}>
-      <planeBufferGeometry args={[10, 10]} />
+      <planeBufferGeometry args={[20, 20]} />
       <meshBasicMaterial map={texture} />
     </mesh>
   )
