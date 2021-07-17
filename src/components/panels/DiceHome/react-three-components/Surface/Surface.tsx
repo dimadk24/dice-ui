722ee43213f90cf1e2be53357af73d643e3c9a98
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-07-17 20:23:58 +0300

    perf: check without surface texture

--- src/components/panels/DiceHome/react-three-components/Surface/Surface.tsx
+++ src/components/panels/DiceHome/react-three-components/Surface/Surface.tsx
@@ -1,21 +1,21 @@
 import { usePlane } from '@react-three/cannon'
-import { RepeatWrapping, TextureLoader } from 'three'
-import woodenTableTextureUrl from './black-wood.jpeg'
-import { useLoader } from '@react-three/fiber'
+// import { RepeatWrapping, TextureLoader } from 'three'
+// import woodenTableTextureUrl from './black-wood.jpeg'
+// import { useLoader } from '@react-three/fiber'
 
 export function Surface(): JSX.Element {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, 0, 0],
   }))
-  const texture = useLoader(TextureLoader, woodenTableTextureUrl)
-  texture.wrapS = RepeatWrapping
-  texture.repeat.set(4, 1)
+  // const texture = useLoader(TextureLoader, woodenTableTextureUrl)
+  // texture.wrapS = RepeatWrapping
+  // texture.repeat.set(4, 1)
 
   return (
     <mesh ref={ref}>
       <planeBufferGeometry args={[20, 20]} />
-      <meshStandardMaterial map={texture} />
+      <meshStandardMaterial />
     </mesh>
   )
 }
