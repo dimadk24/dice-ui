3c0c7e0a05b49d11704bc2c7c78b947a8062e9a9
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-07-31 22:38:32 +0300

    fix: dynamically set walls position based on screen size

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,5 @@
+import { MathUtils } from 'three'
+
 export class Utils {
   static isProductionMode = process.env.NODE_ENV === 'production'
 
@@ -12,5 +14,25 @@ export class Utils {
 
   static generateRandomWithRandomSign(min: number, max: number): number {
     return Utils.changeSignRandomly(Utils.generateRandom(min, max))
+  }
+
+  static getFovSize({
+    cameraFov,
+    cameraAspect,
+    xDistance = 0,
+    yDistance = 0,
+    zDistance = 0,
+  }: {
+    cameraFov: number
+    cameraAspect: number
+    xDistance?: number
+    yDistance?: number
+    zDistance?: number
+  }): number {
+    const vFOV = MathUtils.degToRad(cameraFov) // convert vertical fov to radians
+    // camera distance from the point
+    const distance = Math.sqrt(xDistance ** 2 + yDistance ** 2 + zDistance ** 2)
+    const height = 2 * Math.tan(vFOV / 2) * distance // visible height
+    return height * cameraAspect // visible width
   }
 }
--- src/components/panels/DiceHome/DiceHome.tsx
+++ src/components/panels/DiceHome/DiceHome.tsx
@@ -1,26 +1,41 @@
 import { Canvas } from '@react-three/fiber'
-import { Suspense, useRef } from 'react'
+import { Suspense, useRef, useState } from 'react'
 import { Physics } from '@react-three/cannon'
 import { Surface } from './react-three-components/Surface/Surface'
 import { Dice, DiceRef } from './react-three-components/Dice/Dice'
 import { DebugInDev } from './react-three-components/DebugInDev'
 import styles from './DiceHome.module.css'
-import { Euler, MathUtils } from 'three'
+import { Euler, MathUtils, PerspectiveCamera } from 'three'
 import { Walls } from './react-three-components/Walls/Walls'
+import { Utils } from '../../../Utils'
+
+const cameraYDistance = 3.5
+const cameraZDistance = 4.5
 
 function DiceHome(): JSX.Element {
   const diceRef = useRef<DiceRef>(null)
+  const [fovWidth, setFovWidth] = useState(0)
+
   return (
     <div className={styles.canvasWrapper}>
       <Canvas
-        onCreated={(threeState) => {
+        onCreated={({ gl, camera }) => {
           // eslint-disable-next-line no-param-reassign
-          threeState.gl.physicallyCorrectLights = true
-          threeState.camera.setRotationFromEuler(
+          gl.physicallyCorrectLights = true
+          const perspectiveCamera = camera as PerspectiveCamera
+          perspectiveCamera.setRotationFromEuler(
             new Euler(MathUtils.degToRad(-60), 0, 0)
           )
-          threeState.camera.translateY(3.5)
-          threeState.camera.translateZ(4.5)
+          perspectiveCamera.translateY(cameraYDistance)
+          perspectiveCamera.translateZ(cameraZDistance)
+          setFovWidth(
+            Utils.getFovSize({
+              cameraFov: perspectiveCamera.fov,
+              cameraAspect: perspectiveCamera.aspect,
+              yDistance: cameraYDistance,
+              zDistance: cameraZDistance,
+            })
+          )
         }}
         gl={{ antialias: true }}
         onPointerMissed={() => diceRef.current?.roll()}
@@ -46,7 +61,7 @@ function DiceHome(): JSX.Element {
               />
               <Dice ref={diceRef} />
               <Surface />
-              <Walls />
+              <Walls viewWidth={fovWidth} />
             </DebugInDev>
           </Physics>
         </Suspense>
