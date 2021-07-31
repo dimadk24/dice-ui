af25ce45ee188af3b0f9c9203b3f6725aeb2e1db
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-07-31 22:38:32 +0300

    fix: dynamically set walls position based on screen size

--- src/components/panels/DiceHome/react-three-components/Walls/Walls.tsx
+++ src/components/panels/DiceHome/react-three-components/Walls/Walls.tsx
@@ -1,7 +1,12 @@
 import { Wall } from '../Wall/Wall'
 import { MathUtils } from 'three'
 
-export function Walls(): JSX.Element {
+interface WallsProps {
+  viewWidth: number
+}
+
+export function Walls({ viewWidth }: WallsProps): JSX.Element {
+  const wallsOffset = viewWidth / 2
   return (
     <>
       <Wall rotation={[0, 0, 0]} position={[0, 0, -7]} key="top" />
@@ -12,12 +17,12 @@ export function Walls(): JSX.Element {
       />
       <Wall
         rotation={[0, MathUtils.degToRad(115), 0]}
-        position={[-6.5, 0, 0]}
+        position={[-wallsOffset, 0, 0]}
         key="left"
       />
       <Wall
         rotation={[0, MathUtils.degToRad(-115), 0]}
-        position={[6.5, 0, 0]}
+        position={[wallsOffset, 0, 0]}
         key="right"
       />
       <Wall rotation={[Math.PI / 2, 0, 0]} position={[0, 6, 0]} key="ceiling" />
