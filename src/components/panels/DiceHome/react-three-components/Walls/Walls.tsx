87c8c81519e63545f62a4ed179b728588e383304
Dima Krutolianov <DimaKrutDK@ya.ru>
2021-08-01 10:09:28 +0300

    feat: add walls to top sides

--- src/components/panels/DiceHome/react-three-components/Walls/Walls.tsx
+++ src/components/panels/DiceHome/react-three-components/Walls/Walls.tsx
@@ -6,7 +6,8 @@ interface WallsProps {
 }
 
 export function Walls({ viewWidth }: WallsProps): JSX.Element {
-  const wallsOffset = viewWidth / 2
+  const bottomWallsOffset = viewWidth / 2
+  const topWallsOffset = bottomWallsOffset * 1.3
   return (
     <>
       <Wall rotation={[0, 0, 0]} position={[0, 0, -7]} key="top" />
@@ -17,13 +18,23 @@ export function Walls({ viewWidth }: WallsProps): JSX.Element {
       />
       <Wall
         rotation={[0, MathUtils.degToRad(115), 0]}
-        position={[-wallsOffset, 0, 0]}
-        key="left"
+        position={[-bottomWallsOffset, 0, 0]}
+        key="bottom-left"
       />
       <Wall
         rotation={[0, MathUtils.degToRad(-115), 0]}
-        position={[wallsOffset, 0, 0]}
-        key="right"
+        position={[bottomWallsOffset, 0, 0]}
+        key="bottom-right"
+      />
+      <Wall
+        rotation={[0, MathUtils.degToRad(65), 0]}
+        position={[-topWallsOffset, 0, 0]}
+        key="top-left"
+      />
+      <Wall
+        rotation={[0, MathUtils.degToRad(-65), 0]}
+        position={[topWallsOffset, 0, 0]}
+        key="top-right"
       />
       <Wall rotation={[Math.PI / 2, 0, 0]} position={[0, 6, 0]} key="ceiling" />
     </>
